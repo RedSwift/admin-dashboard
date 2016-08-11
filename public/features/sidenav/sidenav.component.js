8f10d3c402aaf7bfad2c3179cc38576c0867d201
Dominic <jxdlam@gmail.com>
2016-08-11 20:07:16 +0800

    client: added logout

--- public/features/sidenav/sidenav.component.js
+++ public/features/sidenav/sidenav.component.js
@@ -2,12 +2,16 @@
 angular.module('adminApp')
   .component('sideNav', {
     templateUrl: '/features/sidenav/sidenav.template.html',
-    controller: function ($mdSidenav, $scope, $location) {
+    controller: function ($mdSidenav, $scope, $location, $window) {
       var contz = $location.absUrl().split('/')
       this.url = contz[4].toUpperCase()
       this.isSidenavOpen = false
       this.openLeftMenu = () => {
         $mdSidenav('left').toggle()
+      }
+      this.logout = () => {
+        $window.localStorage.clear()
+        $window.location.href = '/#/login'
       }
     }
   })
