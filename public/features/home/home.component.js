548df04ab58959106034a9eb9444b7151f09c77f
Dominic <jxdlam@gmail.com>
2016-08-12 00:31:13 +0800

    adjusted test files

--- public/features/home/home.component.js
+++ public/features/home/home.component.js
@@ -2,7 +2,7 @@
 angular.module('adminApp')
   .component('home', {
     templateUrl: '/features/home/home.template.html',
-    controller: function () {
+    controller: function ($window) {
       if (!$window.localStorage.email || !$window.localStorage.auth_token) $window.location.href = '/#/'
     }
   })
